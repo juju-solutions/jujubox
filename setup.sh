dc229decec356170b654af450995541026887fdf
Whit Morriss <whit.morriss@canonical.com>
2015-02-10 11:32:51 -0600

    add some py dep stuff

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ apt-get update -qq
 apt-get install -qy lxc iptables
 apt-get -qy install juju-core juju-quickstart juju-deployer
 apt-get -qy install tmux charm-tools juju-local openssh-client
-apt-get -qy install virtualenvwrapper
+apt-get -qy install virtualenvwrapper python-dev cython git
 
 useradd -m ubuntu
 echo "ubuntu ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/juju-users
