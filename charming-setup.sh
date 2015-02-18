25e41ea5a45b292e95c2028093970e0a5753c4f4
Whit Morriss <whit.morriss@canonical.com>
2015-02-18 09:57:46 -0600

    tweak

--- charming-setup.sh
+++ charming-setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -e
 
 HOME=/home/ubuntu
 
 git clone https://github.com/juju/plugins.git ${HOME}/.juju-plugins
-
-chown -R ubuntu:ubuntu ${HOME}
 
 RC=${HOME}/.bashrc
 echo "export JUJU_HOME=${HOME}/.juju" >> $RC
 echo "export JUJU_REPOSITORY=${HOME}" >> $RC
 echo "export PROJECT_HOME=${HOME}" >> $RC
 echo "export PATH=$PATH:${HOME}/.juju-plugins" >> $RC
+
+chown -R ubuntu:ubuntu ${HOME}
