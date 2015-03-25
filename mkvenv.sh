b2f67893b6ff3277ddb18e7557d5c471d13ed4b0
Cory Johns <johnsca@gmail.com>
2015-03-24 20:52:03 -0600

    Removed virtualenv from charmbox, as it causes issues with some tests

--- mkvenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-HOME=/home/ubuntu
-
-virtualenv /home/ubuntu/.virtualenvs/charm-review
-. ${HOME}/.virtualenvs/charm-review/bin/activate
-pip install bundletester flake8
-
-chown -R ubuntu:ubuntu ${HOME}
