814593cecd41cffe90dc970212a8dc28cb9486cf
Tim Van Steenburgh <tvansteenburgh@gmail.com>
2015-06-12 10:47:28 -0400

    Add juju-local dep

--- setup.sh
+++ setup.sh
@@ -7,6 +7,10 @@ apt-get update -qq
 
 ## don't install lxc & local till we get local support
 #apt-get install -qy lxc iptables juju-local
+
+## actually, we need juju-local since the local provider
+## can be used by bootstrapping from outside the container
+apt-get -qy install juju-local
 apt-get -qy install juju-core juju-quickstart juju-deployer
 apt-get -qy install tmux charm-tools openssh-client
 apt-get -qy install virtualenvwrapper python-dev cython git
