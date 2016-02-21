dfd8c499cd81e8930986610a2d570d842cc452c5
Charles Butler <chuck@dasroot.net>
2016-02-21 12:33:08 -0500

    introduce new environment variable to support juju 2.0 JUJU_DATA value

--- charming-setup.sh
+++ charming-setup.sh
@@ -9,7 +9,9 @@ RC=${HOME}/.bashrc
 echo "export JUJU_HOME=${HOME}/.juju" >> $RC
 echo "export JUJU_REPOSITORY=${HOME}" >> $RC
 echo "export PROJECT_HOME=${HOME}" >> $RC
+echo "export JUJU_DATA=${HOME}/.local/share/juju" >> $RC
+
 echo "export PATH=$PATH:${HOME}/.juju-plugins" >> $RC
-echo "echo 'welcome to juju'" >> $RC
+echo "echo 'welcome to juju 2.0'" >> $RC
 
 chown -R ubuntu:ubuntu ${HOME}
