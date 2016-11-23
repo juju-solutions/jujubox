39f883648089f5ed540ba959b8797fcb07f4a9c4
Matt Bruzek <matthew.bruzek@canonical.com>
2016-11-23 11:52:49 -0600

    Removing unused packages, and disabling strict host checking.

--- setup.sh
+++ setup.sh
@@ -10,24 +10,30 @@ fi
 # Refresh the potentially stale apt cache.
 apt-get update -qq
 # Install software tools such as add-apt-repository.
-apt-get install -qy software-properties-common
+apt-get install -y software-properties-common
 # Install the ppa for Juju and charm tools.
 apt-add-repository -u -y ppa:juju/stable
 
 # Install juju and the bare minimum components.
 apt-get install -qy \
     byobu \
-    cython \
-    git \
     juju \
     openssh-client \
-    python-dev \
+    python \
+    python3 \
     sudo \
-    vim \
-    virtualenvwrapper
+    vim
 
 # Add the JUJU_USER to the passwordless sudo file.
 echo "${JUJU_USER} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/juju-users
+
+# Disable strict host key checking as Juju hostnames may be reused.
+mkdir -p /home/$JUJU_USER/.ssh
+chmod 700 /home/$JUJU_USER/.ssh
+echo 'Host *' > /home/$JUJU_USER/.ssh/config
+echo '  StrictHostKeyChecking no' >> /home/$JUJU_USER/.ssh/config
+chmod 400 /home/$JUJU_USER/.ssh/config
+chown -R $JUJU_USER:$JUJU_USER /home/$JUJU_USER/.ssh
 
 # Get the version of Juju installed.
 JUJU_VERSION=`juju version`
@@ -36,8 +42,7 @@ JUJU_VERSION=`juju version`
 RC=/home/$JUJU_USER/.bashrc
 echo "echo Welcome to jujubox version ${JUJU_VERSION}" >> $RC
 
-# Cleanup unnecessary packages.
-apt-get remove -qy cython gcc
+# Cleanup any unnecessary packages.
 apt-get autoremove -qy
 apt-get autoclean -qy
 apt-get clean -qy
