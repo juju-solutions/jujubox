f042136fa07cbb453b3adff302dfd4c22b4344fc
Kevin W Monroe <kevin.monroe@canonical.com>
2020-01-24 11:48:01 -0600

    bump devel to focal (need to hold lxd at bionic to prevent snapd dep)

--- setup.sh
+++ setup.sh
@@ -12,17 +12,32 @@ apt-get update -qq
 # Install software tools such as add-apt-repository.
 apt-get install -y software-properties-common
 # devel ppa is required for juju2 beta release
-apt-add-repository -u -y ppa:juju/devel
+# NB: forgive me for what i'm about to do. juju needs lxd, and lxd
+# in focal pulls in snapd, which doesn't work under docker; go bionic.
+cat <<EOF > /etc/apt/sources.list.d/juju-lxd-devel-bionic.list
+deb http://archive.ubuntu.com/ubuntu/ bionic main
+deb http://ppa.launchpad.net/juju/devel/ubuntu bionic main
+EOF
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 376A290EC8068B11
+apt-get update -qq
 
 # Install juju and the bare minimum components.
 apt-get install -qy \
     byobu \
     juju \
+    lxd=3.0.0-0ubuntu4 lxd-client=3.0.0-0ubuntu4 \
     openssh-client \
     python \
     python3 \
     sudo \
     vim
+
+# Keep lxd on the old bionic version
+echo 'lxd hold' | dpkg --set-selections
+echo 'lxd-client hold' | dpkg --set-selections
+
+# Fix sudo: https://bugzilla.redhat.com/show_bug.cgi?id=1773148
+echo 'Set disable_coredump false' > /etc/sudo.conf
 
 # Add the JUJU_USER to the passwordless sudo file.
 echo "${JUJU_USER} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/juju-users
